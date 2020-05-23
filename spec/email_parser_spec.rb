2a5386e6b60a295e9aa708d30caee1a9b66750cb
Romena Murgor <romenamurgor@gmail.com>
2020-05-23 17:57:04 +0000

    Done.

--- spec/email_parser_spec.rb
+++ spec/email_parser_spec.rb
@@ -1,20 +1,14 @@
-describe "EmailAddressParser" do
-  describe '#parse' do
-    it "parses CSV emails" do
-      expect(EmailAddressParser.new("avi@test.com, arel@test.com").parse).to eq(["avi@test.com", "arel@test.com"])
-    end
+class EmailParser
 
-    it "parses space delimited emails" do
-      expect(EmailAddressParser.new("avi@test.com arel@test.com").parse).to eq(["avi@test.com", "arel@test.com"])
-    end
-
-    it "parses both CSV and space delimited emails" do
-      emails = "avi@test.com, arel@test.com test@avi.com, test@arel.com"
-      expect(EmailAddressParser.new(emails).parse).to eq(["avi@test.com", "arel@test.com","test@avi.com", "test@arel.com"]) 
-    end
-
-    it 'parses and removes duplicate emails' do
-      expect(EmailAddressParser.new("avi@test.com, avi@test.com").parse).to eq(["avi@test.com"])
-    end
+  attr_accessor :email
+  def  initialize(emails)
+    @email = emails
   end
+
+  def parse
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
+  end
+
 end
